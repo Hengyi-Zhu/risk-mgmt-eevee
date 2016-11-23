4bbb568459cfcc5d284fdd49e6386bf7c2108a1f
Hengyi-Zhu <hengyi.harry.zhu@gmail.com>
2016-11-23 10:58:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request, redirect
+import pandas as pd
+import numpy as np
+import requests
+import datatime
+import dill, os
 
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-  return redirect('/index')
+def main():    
+    return redirect('/index')
 
-@app.route('/index')
-def index():
-  return render_template('index.html')
+@app.route('/index', methods=['GET', 'POST'])
+def index():    
+    return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    port = int(os.environ.get("PORT", 5000))    
+    app.run(host='0.0.0.0', port=port)
