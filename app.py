fcb484edf8f4da3c870dd07b1226c0e013988a66
Harry Zhu <hengyi.harry.zhu@gmail.com>
2016-11-23 16:44:18 -0500

    harry

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ from flask import Flask, render_template, request, redirect
 import pandas as pd
 import numpy as np
 import requests
-import datatime
+import datetime
 import dill, os
 
 app = Flask(__name__)
 
 @app.route('/')
-def main():    
+def main():
     return redirect('/index')
 
 @app.route('/index', methods=['GET', 'POST'])
-def index():    
+def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))    
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
